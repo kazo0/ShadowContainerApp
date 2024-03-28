0669fc32387f57d8f9e9d0c620c42a9b882a5e74
Steve Bilogan <steve.bilogan@gmail.com>
2024-03-28 10:13:39 -0400

    alternating shadows

--- ShadowContainerApp/MainPage.xaml.cs
+++ ShadowContainerApp/MainPage.xaml.cs
@@ -2,8 +2,13 @@ namespace ShadowContainerApp;
 
 public sealed partial class MainPage : Page
 {
-    public MainPage()
-    {
-        this.InitializeComponent();
-    }
+	public MainPage()
+	{
+		this.InitializeComponent();
+	}
+
+	private void Image_Tapped(object sender, Microsoft.UI.Xaml.Input.TappedRoutedEventArgs e)
+	{
+		MyShadowContainer.Shadows = MyShadowContainer.Shadows == AlternateShadows ? Shadows : AlternateShadows;
+	}
 }
